bd82e6d4f28c3d3427e76d659de345393e91e9cf
Jay Thakkar <jay.thakkar0514@gmail.com>
2025-08-22 00:35:03 +1000

    updated fractal

--- fractal.py
+++ fractal.py
@@ -1,3 +1,7 @@
+############################################################################
+# BURNING SHIP #############################################################
+############################################################################
+
 import torch
 import numpy as np
 # Device configuration
@@ -6,21 +10,11 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 x_min, x_max = -2.0, 1.0
 y_min, y_max = -2.0, 1.0
 
-
-# image res (higher for more detail, but slower)
-width = 1000
-height = 1000
-
 # higher iterations capture more detail, but take longer and use more memory
 max_iter = 200
 
-# calc  step size based on the coord range and res.
-x_step = (x_max - x_min) / width
-y_step = (y_max - y_min) / height
 
-
-
-Y, X = np.mgrid[y_min:y_max:y_step, x_min:x_max:x_step]
+Y, X = np.mgrid[y_min:y_max:0.005, x_min:x_max:0.005]
 
 
 c_real = torch.Tensor(X)
@@ -36,10 +30,6 @@ ns = torch.zeros_like(cs, dtype=torch.float32)
 cs = cs.to(device)
 zs = zs.to(device)
 ns = ns.to(device)
-
-print(f"Computing a {width}x{height} Burning Ship fractal with {max_iter} iterations...")
-
-
 
 for i in range(max_iter):
     # The core Burning Ship iteration: z_n+1 = (|real(z_n)| + i|imag(z_n)|)^2 + c
@@ -61,7 +51,7 @@ for i in range(max_iter):
     zs = zs_
 
 import matplotlib.pyplot as plt
-fig = plt.figure(figsize=(16,10))
+fig = plt.figure(figsize=(10,6))
 def processFractal(a):
     """Display an array of iteration counts as a
     colorful picture of a fractal."""
@@ -77,4 +67,75 @@ def processFractal(a):
 plt.imshow(processFractal(ns.cpu().numpy()),  extent=[x_min, x_max, y_min, y_max])
 plt.tight_layout(pad=0)
 plt.show()
+
+############################################################################
+# NEWTONS FRATCAL ##########################################################
+############################################################################
+
+# # import torch
+# import numpy as np
+# # Device configuration
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# x_min, x_max = -1.5, 1.5
+# y_min, y_max = -1.5, 1.5
+
+# # higher iterations capture more detail, but take longer and use more memory
+# max_iter = 200
+
+# Y, X = np.mgrid[y_min:y_max:0.005, x_min:x_max:0.005]
+
+# # load into PyTorch tensors
+# x = torch.Tensor(X)
+# y = torch.Tensor(Y)
+# z = torch.complex(x, y)          # initial z for newtons fractal
+# zs = z.clone()                   # working variable that will hold z_n
+# ns = torch.zeros_like(x)         # counter/"escape time" proxy
+
+# # transfer to the GPU device
+# z = z.to(device)
+# zs = zs.to(device)
+# ns = ns.to(device)
+
+
+
+# # --- NEWTON FRACTAL: (f(z)=z^3 - 1) ---
+# tol = 1e-6                       # convergence tolerance
+# eps = torch.tensor(1e-12, device=device)  # derivative safety
+
+# for i in range(max_iter):
+#     # f = zs*zs*zs - 2*zs + 2
+#     # f_dash = 3*zs*zs - 2
+#     f = zs*zs*zs - 1
+#     f_dash = 3*zs*zs 
+
+#     # tiny bias in the denominator to avoid division by 0
+#     zs = zs - f / (f_dash + eps)
+
+#     # Count iterations until converged
+#     not_converged = torch.abs(f) > tol
+#     #Update variables to compute
+#     ns += not_converged.to(ns.dtype)
+
+# #plot
+
+# import matplotlib.pyplot as plt
+# fig = plt.figure(figsize=(16,10))
+# def processFractal(a):
+#     """Display an array of iteration counts as a
+#     colorful picture of a fractal."""
+#     a_cyclic = (6.28*a/20.0).reshape(list(a.shape)+[1])
+#     img = np.concatenate([10+20*np.cos(a_cyclic),
+#     30+50*np.sin(a_cyclic),
+#     155-80*np.cos(a_cyclic)], 2)
+#     img[a==a.max()] = 0
+#     a = img
+#     a = np.uint8(np.clip(a, 0, 255))
+#     return a
+
+# # plt.imshow(processFractal(ns.cpu().numpy()),  extent=[x_min, x_max, y_min, y_max])
+# plt.imshow(ns.cpu().numpy(),  extent=[x_min, x_max, y_min, y_max],  cmap='inferno')
+
+# plt.tight_layout(pad=0)
+# plt.show()
 
